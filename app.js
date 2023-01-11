68f755d772e7138d78e3fbc6db4a6e1a2f81df96
Lia Graham <liavibanez@gmail.com>
2023-01-10 20:31:56 -0600

    adding search term functionality

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const movieButton = document.getElementById('Movie');
 const seriesButton = document.getElementById('Series');
 const searchResults = document.querySelector('.container div');
-const movieUrl = "https://www.omdbapi.com/?s=house&type=movie&apikey=274cb9ed";
+let searchTerm;
 const seriesUrl = "https://www.omdbapi.com/?s=house&type=series&apikey=274cb9ed";
 
 movieButton.addEventListener('click', getMovieResults);
 
 async function getMovieResults(){
+  let searchTerm = document.getElementById('search').value
+  let movieUrl = "https://www.omdbapi.com/?s="+searchTerm+"&type=movie&apikey=274cb9ed";
   const tvData = await fetch(movieUrl,{
     headers:{
       'Accept':'application/json',
@@ -16,12 +18,14 @@ async function getMovieResults(){
     console.log(tvDataObj);
 
     //the below code shows the title of the first entry in tvDataObj 
-    // document.getElementById('results').innerHTML = JSON.stringify(tvDataObj.Search[0].Title)
+    // document.getElementById('results').innerHTML = JSON.stringify(tvDataObj.Search[9].Title)
 }
 
 seriesButton.addEventListener('click',getSeriesResults)
 
 async function getSeriesResults(){
+  let searchTerm = document.getElementById('search').value
+  let seriesUrl = "https://www.omdbapi.com/?s="+searchTerm+"&type=series&apikey=274cb9ed";
   const tvData = await fetch(seriesUrl,{
     headers:{
       'Accept':'application/json',
