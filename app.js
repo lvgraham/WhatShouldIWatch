94e9d6a9517cf5cf45184b62668a354dd2f35a70
Lia Graham <liavibanez@gmail.com>
2023-01-10 20:11:47 -0600

    adding functionality to series button

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const movieButton = document.getElementById('Movie');
 const seriesButton = document.getElementById('Series');
 const searchResults = document.querySelector('.container div');
-const movieUrl = "https://www.omdbapi.com/?i=tt3896198&apikey=274cb9ed";
-const seriesUrl = "https://www.omdbapi.com/?i=tt3896198&apikey=274cb9ed";
+const movieUrl = "https://www.omdbapi.com/?s=house&type=movie&apikey=274cb9ed";
+const seriesUrl = "https://www.omdbapi.com/?s=house&type=series&apikey=274cb9ed";
 
 movieButton.addEventListener('click', getMovieResults);
 
@@ -14,17 +14,23 @@ async function getMovieResults(){
     const tvDataObj = await tvData.json();
     console.log(tvData);
     console.log(tvDataObj);
+
+    //the below code shows the title of the first entry in tvDataObj 
+    // document.getElementById('results').innerHTML = JSON.stringify(tvDataObj.Search[0].Title)
 }
 
 seriesButton.addEventListener('click',getSeriesResults)
 
 async function getSeriesResults(){
-  const tvData = await fetch(movieUrl,{
+  const tvData = await fetch(seriesUrl,{
     headers:{
       'Accept':'application/json',
     } });
     const tvDataObj = await tvData.json();
     console.log(tvData);
     console.log(tvDataObj);
+
+    //the below code shows all data in tvDataObj 
+    // document.getElementById('results').innerHTML = JSON.stringify(tvDataObj);
 }
 
